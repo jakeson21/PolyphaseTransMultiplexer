354247bd4817c59979b6afc62bd3769d2f5161d6
Jacob Miller <jakeson21@users.noreply.github.com>
2020-02-05 14:07:01 -0500

    Update polyphase_txmultiplexer.py

--- channelizer/polyphase_txmultiplexer.py
+++ channelizer/polyphase_txmultiplexer.py
@@ -5,21 +5,38 @@ import json
 import pdb
 
 
+def make_polyphase_filter(channels, order=150):
+    fs = channels
+    cutoff = 0.5
+    transition_width = 0.499
+    numtaps, beta = signal.kaiserord(order, width=transition_width / (0.5 * fs))
+    # print('fs={}, transition_width={}, cutoff={}, numtaps={}, beta={}'.format(fs, transition_width, cutoff, numtaps, beta))
+
+    h = signal.firwin(numtaps, cutoff=cutoff, window=('kaiser', beta), scale=False, nyq=0.5 * fs)
+    h[abs(h) <= 1e-15] = 0.
+    h = h / np.max(abs(h))
+
+    # form Hk
+    N: int = int(np.ceil(np.ceil(h.size / fs) * fs))
+    h.resize((N,), refcheck=False)
+    # Columns are filter paths
+    hp = np.reshape(h, (-1, fs)).transpose()
+    return hp
+
+
 class PolyphaseTxMultiplexer:
     def __init__(self, sample_rate_Hz: float, channel_bandwidth_Hz: float, block_size: int):
         self.sample_rate_Hz: float = sample_rate_Hz
         self.channel_bandwidth_Hz: float = channel_bandwidth_Hz
         self.blockSize = block_size
+        self.t_last = 0
 
         # Ensure total bandwidth is evenly divisible by the channel bandwidth
         if (sample_rate_Hz / channel_bandwidth_Hz) != np.round(sample_rate_Hz / channel_bandwidth_Hz):
-            raise ValueError(
-                'sample_rate_Hz ({}) must be evenly divisible by channel_bandwidth_Hz ({})'.format(sample_rate_Hz,
-                                                                                                   channel_bandwidth_Hz))
+            raise ValueError('sample_rate_Hz ({}) must be evenly divisible by channel_bandwidth_Hz ({})'.format(sample_rate_Hz, channel_bandwidth_Hz))
 
         self.M = int(self.sample_rate_Hz / self.channel_bandwidth_Hz)
-        order = 10
-        self.filter = self.make_polyphase_filter(order=order)
+        self.filter = make_polyphase_filter(self.M)
         # inputBuffer is used to save filterlength-1 WB samples for overlap-and-save based filtering
         self.input_buffer = np.zeros((self.M, self.filter.shape[1] - 1), dtype=np.cdouble)
 
@@ -31,30 +48,9 @@ class PolyphaseTxMultiplexer:
         d['filter'] = self.filter.tolist()
         return json.dumps(d)
 
-    def make_polyphase_filter(self, order):
-        fs = self.M
-        # cutoff = 1.0/(self.M - 0.25*self.M)
-        cutoff = 0.5
-        transition_width = 0.1
-        numtaps, beta = signal.kaiserord(150, width=transition_width / (0.5 * fs))
-        print('fs={}, transition_width={}, cutoff={}, numtaps={}, beta={}'.format(fs, transition_width, cutoff, numtaps,
-                                                                                  beta))
-
-        h = signal.firwin(numtaps, cutoff=cutoff, window=('kaiser', beta), scale=False, nyq=0.5 * self.M)
-        h[abs(h) <= 1e-15] = 0.
-        h = h / np.max(abs(h))
-
-        # form Hk
-        N: int = int(np.ceil(np.ceil(h.size / self.M) * self.M))
-        h.resize((N,), refcheck=False)
-        # Columns are filter paths
-        Hk = np.reshape(h, (-1, self.M)).transpose()
-        return Hk
-
     def polyphase_up_fir(self, x):
         # Filter by Overlap and save
-        xf = np.asarray([signal.lfilter(self.filter[n, :], 1, np.hstack((self.input_buffer[n, :], x[n, :]))) for n in
-                         range(self.M)])
+        xf = np.asarray([signal.lfilter(self.filter[n, :], 1, np.hstack((self.input_buffer[n, :], x[n, :]))) for n in range(self.M)])
         # Save tail end for next time
         self.input_buffer = x[:, -self.input_buffer.shape[1]:]
         # take second part
@@ -65,6 +61,10 @@ class PolyphaseTxMultiplexer:
     def process(self, x):
         y = np.fft.ifft(x, n=self.M, axis=0) * self.M
         output = self.polyphase_up_fir(y)
+        if self.M % 2 == 0:
+            t = np.linspace(0, output.size, output.size, endpoint=True) + self.t_last
+            self.t_last = t[-1] + t[1]-t[0]
+            output *= np.exp(2j * np.pi / (2 * self.M) * t)
         return output
 
 
