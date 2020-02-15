dfdde7891d4f2f35414df8223e4edd56bccdd56b
jakeson21 <jake_son@yahoo.com>
2020-02-14 22:41:00 -0500

    Working octave filter bank. Needs better PR filter.

--- run_polyphase_test.py
+++ run_polyphase_test.py
@@ -7,92 +7,8 @@ import sys
 from transmux.polyphase_rxchannelizer import PolyphaseRxChannelizer
 from transmux.polyphase_txmultiplexer import PolyphaseTxMultiplexer
 
-from transmux.halfband import Synthesis, Analysis
-
-
-def rcosdesign(beta, span, sps, name='normal'):
-    delay = span * sps / 2
-    t = np.arange(-delay, delay + 1) / sps
-    if name == 'normal':
-        # Design a normal raised cosine filter
-        b = np.zeros((t.size,))
-        # Find non-zero denominator indices
-        denom = 1 - (2 * beta * t) ** 2
-        idx1 = np.abs(denom) > np.sqrt(sys.float_info.epsilon)
-
-        # Calculate filter response for non-zero denominator indices
-        b[idx1] = np.sinc(t[idx1]) * (np.cos(np.pi * beta * t[idx1]) / denom[idx1]) / sps
-
-        # fill in the zeros denominator indices
-        b[idx1 == False] = beta * np.sin(np.pi / (2 * beta)) / (2 * sps)
-    else:
-        # Design a square root raised cosine filter
-        b = np.zeros((t.size,))
-        # Find mid-point
-        idx1 = t == 0
-        if np.any(idx1):
-            b[idx1] = -1 / (np.pi * sps) * (np.pi * (beta - 1) - 4 * beta)
-
-        # Find non-zero denominator indices
-        idx2 = np.abs(np.abs(4. * beta * t) - 1.0) < np.sqrt(sys.float_info.epsilon)
-        if np.any(idx2):
-            b[idx2] = 1 / (2 * np.pi * sps) \
-                      * (np.pi * (beta + 1) * np.sin(np.pi * (beta + 1) / (4 * beta))
-                         - 4 * beta * np.sin(np.pi * (beta - 1) / (4 * beta))
-                         + np.pi * (beta - 1) * np.cos(np.pi * (beta - 1) / (4 * beta)))
-
-        # fill in the zeros denominator indices
-        nind = t[idx1 + idx2 == False]
-
-        b[idx1 + idx2 == False] = (-4 * beta / sps) * (np.cos((1 + beta) * np.pi * nind) + np.sin((1 - beta) * np.pi * nind) / (4 * beta * nind)) / (np.pi * ((4 * beta * nind) ** 2 - 1))
-
-    # Normalize filter energy
-    b = b / np.sqrt(np.sum(b ** 2))
-    return b
-
-
-def gen_complex_chirp(fs=44100, duration=1.):
-    f0 = -fs / 2.1
-    f1 = fs / 2.1
-    t1 = duration
-    beta = (f1 - f0) / float(t1)
-    t = np.arange(0, t1, t1 / float(fs))
-    data = np.exp(2j * np.pi * (.5 * beta * (t ** 2) + f0 * t))
-    # Add noise to FDMA signal
-    data += 0.001 * np.squeeze(np.random.randn(data.size, 2).view(np.complex128))
-    return data
-
-
-def gen_fdma(fs, bw):
-    num_chans = int(np.floor(fs / bw))
-    # Generate QPSK data of about 1 second
-    C = np.asarray([1 + 1j, 1 - 1j, -1 + 1j, -1 - 1j])
-    S = C[np.random.randint(C.size, size=(bw*2, 1))]
-    Sup = np.zeros((S.size, num_chans - 1,))
-    x = np.hstack((S, Sup))
-    data = np.squeeze(np.reshape(x, (x.size, -1)))
-    hrrc = np.asarray(rcosdesign(beta=0.3, span=256, sps=num_chans * 2, name='normal'), dtype=np.cdouble)
-    data = signal.lfilter(hrrc, 1, data)
-
-    # generate fdma mapping
-    f = np.linspace(-fs / 2, fs / 2, num_chans * 2 + 1)[1::2]
-    fdma = np.random.randint(num_chans, size=10 * num_chans)
-    bs = int(np.floor(data.size / fdma.size))
-
-    n = 0
-    t = np.arange(0, data.size)
-    for m in t[::bs]:
-        fc = f[fdma[n]]
-        z = np.arange(0, bs, dtype=int) + m
-        p = np.ones_like(np.asarray(data[z]))
-        L = int(np.round(0.05 * bs))
-        p[:L] = np.logspace(-6, 0, num=L)
-        p[-L:] = np.logspace(0, -6, num=L)
-        data[z] *= np.exp(2j * np.pi * fc * t[z] / fs) * p
-        n += 1
-    # Add noise to FDMA signal
-    data += 0.01 * np.squeeze(np.random.randn(data.size, 2).view(np.complex128))
-    return data
+from transmux.halfband import Synthesis, Analysis, OctaveAnalysis
+from transmux.utils import gen_complex_chirp, gen_fdma
 
 
 if __name__ == "__main__":
@@ -108,8 +24,7 @@ if __name__ == "__main__":
     inds = np.arange(0, block_len * num_channels * num_blocks, dtype=int).reshape(num_blocks, -1)
 
     channelHz = Fs / num_channels
-    # rx = PolyphaseRxChannelizer(sample_rate_Hz=Fs, channel_bandwidth_Hz=channelHz)
-    rx = Analysis(Analysis())
+    rx = PolyphaseRxChannelizer(sample_rate_Hz=Fs, channel_bandwidth_Hz=channelHz)
     print(rx)
 
     # Start the stopwatch / counter
@@ -128,8 +43,7 @@ if __name__ == "__main__":
     print("Elapsed time: {} s".format(t1_stop - t1_start))
     print("Samples per second: {}".format(num_blocks*output.size / (t1_stop - t1_start)))
 
-    # tx = PolyphaseTxMultiplexer(sample_rate_Hz=Fs, channel_bandwidth_Hz=channelHz)
-    tx = Synthesis(Synthesis())
+    tx = PolyphaseTxMultiplexer(sample_rate_Hz=Fs, channel_bandwidth_Hz=channelHz)
     print(tx)
     inds = np.arange(0, block_len * num_blocks, dtype=int).reshape(num_blocks, -1)
     t1_start = perf_counter()
--- transmux/halfband.py
+++ transmux/halfband.py
@@ -20,8 +20,7 @@ class PolyphaseDownFir:
         xi = np.vstack((head_data_stream, tail_data_streams[::-1, :]))
 
         # Overlap and save here
-        xf = np.asarray(
-            [signal.lfilter(self.filter[n, :], 1, np.hstack((self.input_buffer[n, :], xi[n, :]))) for n in range(self.M)])
+        xf = np.asarray([signal.lfilter(self.filter[n, :], 1, np.hstack((self.input_buffer[n, :], xi[n, :]))) for n in range(self.M)])
         # Save tail end for next time
         self.input_buffer = xi[:, -self.input_buffer.shape[1]:]
         # take second part
@@ -54,41 +53,89 @@ class PolyphaseUpFir:
 
 # Mux
 class Synthesis:
-    def __init__(self, handle=None):
+    def __init__(self):
         self.M = 2
-        self.filter = make_polyphase_filter(self.M)
+        self.filter = make_polyphase_filter(self.M, order=40, cutoff=0.5, transition_width=0.2)
         self.up_fir = PolyphaseUpFir(self.M, self.filter)
         self.t_last = 0
-        self.handle = handle
 
     def process(self, x):
-        # if self.handle is not None:
-        #     x = self.handle.process(x)
         y = np.fft.ifft(x, n=self.M, axis=0) * self.M
         output = self.up_fir.process(y)
         if self.M % 2 == 0:
             t = np.linspace(0, output.size, output.size, endpoint=True) + self.t_last
             self.t_last = t[-1] + t[1]-t[0]
-            output *= np.exp(2j * np.pi / (2 * self.M) * t)
-        return output
+            d = output * np.exp(-2j * np.pi / (2 * self.M) * t)
+        return d
 
 
 # De-mux
 class Analysis:
-    def __init__(self, handle=None):
+    def __init__(self):
         self.M = 2
-        self.filter = make_polyphase_filter(self.M)
+        self.filter = make_polyphase_filter(self.M, order=40, cutoff=0.5, transition_width=0.2)
         self.down_fir = PolyphaseDownFir(self.M, self.filter)
         self.t_last = 0
-        self.handle = handle
 
     def process(self, x):
-        # if self.handle is not None:
-        #     x = self.handle.process(x)
         if self.M % 2 == 0:
             t = np.linspace(0, x.size, x.size, endpoint=True) + self.t_last
             self.t_last = t[-1] + t[1]-t[0]
-            x *= np.exp(-2j * np.pi / (2 * self.M) * t)
-        y = self.down_fir.process(x)
-        output = np.fft.ifft(y, n=self.M, axis=0) * self.M
+            d = x * np.exp(-2j * np.pi / (2 * self.M) * t)
+        y = self.down_fir.process(d)
+        output = np.fft.ifft(y, n=self.M, axis=0) # * self.M
         return output
+
+
+class OctaveSynthesis:
+    def __init__(self, n=1):
+        assert n > 0, 'n should be > 0'
+        self.num_octaves = n
+        self.octaves = {i: {'handle': Synthesis()} for i in range(0, self.num_octaves)}
+
+    def octave_synthesis(self, x, n, y):
+        if n > 0:
+            # start with last 2 octaves, combine them
+            z = np.vstack((x[str(n-1)], y))
+            # process 2
+            y = self.octaves[n-1]['handle'].process(z)
+            n -= 1
+            y = self.octave_synthesis(x, n, y)
+        return y
+
+    def process(self, x: dict):
+        # process data from each octave
+        # start by grabbing last element, the smallest one
+        y = x[str(self.num_octaves)]
+        y = self.octave_synthesis(x, self.num_octaves, y)
+        return y
+
+
+class OctaveAnalysis:
+    def __init__(self, n=1):
+        assert n > 0, 'n should be > 0'
+        self.num_octaves = n
+        self.octaves = {str(i): {'handle': Analysis(), 'data': None} for i in range(0, self.num_octaves+1)}
+
+    def octave_analysis(self, x, n=0):
+        if n < self.num_octaves-1:
+            y = self.octaves[str(n)]['handle'].process(x)
+            # Store negative frequency
+            self.octaves[str(n)]['data'] = y[1, :]
+            # Process positive frequency
+            n += 1
+            self.octave_analysis(y[0, :], n)
+        else:
+            # Process final octave
+            y = self.octaves[str(n)]['handle'].process(x)
+            self.octaves[str(n)]['data'] = y[1, :]
+            self.octaves[str(n+1)]['data'] = y[0, :]
+
+    def process(self, x):
+        self.octave_analysis(x)
+        # Collect data from each octave
+        y = dict()
+        for k in self.octaves:
+            y[k] = self.octaves[k]['data']
+        return y
+
